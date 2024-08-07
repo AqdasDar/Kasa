5c3ca2953dff81a72c8aa86d208c14fca0a37029
Aqdas Dar <aqdas.dar@epitech.eu>
2024-08-08 00:30:48 +0200

    title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,8 +9,16 @@ function Header() {
         <header className='header'>
             <img className='header__logo' src={Logo} alt='logo Kasa' height="68px" width="211px"/>
             <nav>
-                <NavLink className='header__a' to="/">Accueil</NavLink>
-                <NavLink className='header__a' to="/a_propos">A Propos</NavLink>
+                <NavLink
+                    className={({isActive}) => isActive ? 'header__a active' : 'header__a'}
+                    to="/">
+                    Accueil
+                </NavLink>
+                <NavLink
+                    className={({isActive}) => isActive ? 'header__a active' : 'header__a'}
+                    to="/a_propos">
+                    A Propos
+                </NavLink>
             </nav>
         </header>
     )
